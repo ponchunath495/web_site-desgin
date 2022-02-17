0057580c4119a226129ae4963d04025a6e99245a
ponchu debnath <93307330+ponchunath495@users.noreply.github.com>
2022-02-17 22:46:40 +0600

    html_css_bootstrap_js

---
+++ script/app.js
@@ -0,0 +1,44 @@
+
+
+    
+$(document).ready(function(){
+    $('#product-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        smartSpeed:1000,
+        autoplay:true,
+        nav:true,
+        dots:false, 
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+    $('#testimonial-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false, 
+        autoplay:true,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                items:1 
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+    });
+}) 
